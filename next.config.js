1f091cccf99fbbf721a6bf7e4162dd79687b78f6
Akira <dzirt1907@rambler.ru>
2023-06-16 00:10:54 +0300

    Created songs fetching and list display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['fbbeaonwbzbvfsaeuvjj.supabase.co'],
+  },
+}
 
 module.exports = nextConfig
